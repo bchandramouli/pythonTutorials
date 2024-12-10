e6bb907de5a42a1dab257658d726c5153222ea4c
“bchandramouli” <mouli.biz@gmail.com>
2024-12-10 11:37:32 -0800

    rotating and timed rotating log handlers

--- loggingPythonTutorials.py
+++ loggingPythonTutorials.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3 
 
+# https://docs.python.org/3/library/logging.config.html
+
 #thanks to @patloeber's youtube channel
 
 # allow logging to happen
 import logging
 import logging.config
+import traceback
+import time
 
 # Get the absolute path in place
 from os import path
@@ -35,6 +39,30 @@ logger.critical("Critical message")
 logger.propagate = False
 
 
+try:
+	a = [1, 2, 3]
+	# a[4] = 0 # to force an exception
+except IndexError as e:
+	logger.error("The error is %s", traceback.format_exc())
+	#logger.error(e, exc_infp=True)
+
+# Instead of the below methods of handlers
+# please create a logger.conf - see the that file
+
+# rotating log files
+#from logging.handlers import RotatingFileHandler
+
+# if you want to use a time based rollover
+from logging.handlers import TimedRotatingFileHandler
+
+#handler = RotatingFileHandler('app.log', maxBytes=2000, backupCount=5)
+handler = TimedRotatingFileHandler('timed_app.log', when='s', interval=5, backupCount=5)
+logger.addHandler(handler)
+
+for _ in range(100000):
+	logger.info('error file rotation')
+	time.sleep(5)
+
 # create handlers that logs to a stream
 stream_h = logging.StreamHandler()
 file_h = logging.FileHandler('file.log')
@@ -56,5 +84,3 @@ logger.warning('this is a warning')
 logger.error('this is an error')
 logger.info('this is info')
 
-
-#Instead of the above please create a logger.conf - see the that file
