9bfdd35ef286cb10f4016b0e974386ddd5d21716
“bchandramouli” <mouli.biz@gmail.com>
2024-12-10 11:10:14 -0800

    fixed logging logger keys issue

--- loggingPythonTutorials.py
+++ loggingPythonTutorials.py
@@ -4,9 +4,21 @@
 
 # allow logging to happen
 import logging
+import logging.config
+
+# Get the absolute path in place
+from os import path
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logger.conf')
+print(log_file_path)
+logging.config.fileConfig(log_file_path)
+
+#use the simple example logger from logging.conf
+logger = logging.getLogger('simpleExample')
+logger.debug('Test debug with simpl example logger')
 
 # useful to add the name of the module
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
+
 
 # needs to be setup upfront if you want the debugs turned on
 # once logging starts, config cannot be changed
@@ -31,7 +43,7 @@ file_h = logging.FileHandler('file.log')
 stream_h.setLevel(logging.WARNING)
 file_h.setLevel(logging.ERROR)
 
-#setup formatters
+#setup formatters if the basic config above is not used
 formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s)')
 stream_h.setFormatter(formatter)
 file_h.setFormatter(formatter)
@@ -43,3 +55,6 @@ logger.addHandler(file_h)
 logger.warning('this is a warning')
 logger.error('this is an error')
 logger.info('this is info')
+
+
+#Instead of the above please create a logger.conf - see the that file
