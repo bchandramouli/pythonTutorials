44440b09f8e5160550cbf0b5c6595378be48d83b
“bchandramouli” <mouli.biz@gmail.com>
2024-12-13 11:59:38 -0800

    added brew list

--- 00_pipAndBrewListAndInstall.py
+++ 00_pipAndBrewListAndInstall.py
@@ -4,9 +4,16 @@
 
 import subprocess
 
+"""
+#  assumes the following files are available
+ - .pip.list - obtained earlier from pip3 list > .pip.list
+ - .brew.list - obtained earlier from brew list > .brew.list
+"""
+
 if __name__ == "__main__":
     # subprocess.run(["ls", "-l"])
     pkgs = [line.strip() for line in open(".pip.list", "r")]
+    brews = [line.strip() for line in open(".brew.list", "r")]
     """
     # output of pip list in .pip.list
     # the output first 2 lines has the header info
@@ -17,3 +24,9 @@ if __name__ == "__main__":
         pkg = package.split()[0]
         print(pkg)
         subprocess.run(["pip3", "install", pkg])
+
+    subprocess.run(["brew", "update"])
+
+    for brew in brews:
+        print(brew)
+        subprocess.run(["brew", "install", brew])
